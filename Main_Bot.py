831412446c7f130f7d6a519497f5d50dce66dfb2
JohnLi002 <44849474+JohnLi002@users.noreply.github.com>
2020-11-22 18:11:02 -0500

    added the bare basics to create a discord bot. Must add commands later

--- Main_Bot.py
+++ Main_Bot.py
@@ -1,1 +1,13 @@
 #Bot
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '.')
+
+@client.event
+async def on_ready():
+    print('bot is ready.')
+
+
+
+client.run('NzgwMTU3ODY4MzM4ODM5NTcy.X7rAog.PXYKYtUUB_mIOmoppKLZlftzVSo')
