a4398fa91fc7a598b37c1da41340a8ab3967c14d
JohnLi002 <44849474+JohnLi002@users.noreply.github.com>
2020-12-24 17:16:20 -0500

    Update Main_Bot.py
    Added what I had before

--- Main_Bot.py
+++ Main_Bot.py
@@ -1,13 +1,46 @@
 #Bot
-import discord
+import discord as ds, random
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
-
-@client.event
-async def on_ready():
-    print('bot is ready.')
+#This program is where all the commands will activate in
 
 
+bot = commands.Bot(command_prefix='!')
 
-client.run('Insert Token Here')
+class MyClient(ds.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+
+@commands.command()
+async def multiRoll(ctx, num, dice):
+    rolls = []
+    totalResult = 0
+    count = 0
+    while(count < dice):
+        result = random.randint(1, num)
+        totalResult += result
+        rolls.append(result)
+        count += 1
+    
+    formatRoll = 'You have rolled '
+    if(len(rolls) == 1):
+        formatRoll += '1 die. \nRoll = {'
+    elif(len(rolls) == 0):
+        formatRoll += 'NOTHING!'
+    else:
+        formatRoll += str(len(rolls)) + " dice. \nRolls = {"
+    for r in rolls:
+        formatRoll += str(r)
+        if count != 1:
+            formatRoll += ', '
+        count -= 1
+    formatRoll += "}"
+
+    ctx.send(formatRoll)
+
+client = MyClient()
+client.run('token')
