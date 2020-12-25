a5a67aa87d5dde09b2f787b81c5dd79da8105a40
John Li <lij19@wit.edu>
2020-12-24 22:37:36 -0500

    created a test file in order to figure learn more about how to read and write text documents

---
+++ test.py
@@ -0,0 +1,5 @@
+#Learning to create txt file so that character information can be saved when the bot is not on
+f = open("Saved Files/TestFile.txt", "w") #Should create a filed called Test/File.txt in the folder Saved Files
+
+for i in range(10): #writes into file
+    f.write("line " + i)
